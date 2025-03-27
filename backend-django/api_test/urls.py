39cb8ec430e0f68cfe39d43a90171134de71949c
Mitchell <mosnowitz12@gmail.com>
2025-03-27 12:11:04 -0500

    urls

--- backend-django/api_test/urls.py
+++ backend-django/api_test/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PlayerInfoView, WorkoutView, PlayerPhaseView, save_workout_log, get_workout_logs
+from .views import PlayerInfoView, WorkoutView, PlayerPhaseView, save_workout_log, get_workout_logs, get_players
 
 urlpatterns = [
     path('api/player-info/', PlayerInfoView.as_view(), name='player-info'),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('api/player-phases/<int:player_id>/', PlayerPhaseView.as_view(), name='player-phases'),
     path('api/save-workout-log/', save_workout_log, name='save-workout-log'),
     path('api/get-workout-logs/<int:player_id>/', get_workout_logs, name='get-workout-logs'),
+    path('api/players/', get_players, name='get_players'),
 ]
